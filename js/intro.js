b6bd69504fe870c1b27ae51610b47fae87247340
59013-76-02 <59013-76-02@exchange.ad.afpanet>
2021-02-25 09:19:12 +0100

    ajout de fichier

---
+++ js/intro.js
@@ -0,0 +1,16 @@
+
+        const x=10;
+        console.log(x);
+        let y=20;
+        console.log(y);
+        y=41;
+        console.log(y);
+        let z=y%4;
+        console.log(z);
+        y++;
+        console.log(y);
+        z--;
+        console.log(z);
+        console.log("la valeur de y est: "+y);
+        y+=11;//y=y+11;//
+        y-=3;//y=y-3;//
